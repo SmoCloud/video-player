6d1f0986c860ab2a85218dc69acadb6925f06f80
SmoCloud <psn.power@yahoo.com>
2024-10-07 12:47:02 -0400

    Inserted first video & thumbnail for video, updated styles, added video and thumbnail image

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let videoTitle = document.getElementById("searchButton").onclick = function(){
     videoTitle = document.getElementById("videoTitle").value;
     console.log(videoTitle);
+    if(videoTitle)
+        window.location.href = "player.html";
 }
 
 const player = document.getElementById("vp");
