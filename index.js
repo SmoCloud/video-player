c7bfc2d35690353429986ccbf74265b61f45a8d4
SmoCloud <psn.power@yahoo.com>
2024-10-09 12:51:04 -0400

    Added basic login page, still implementing login logic in JS

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ document.getElementById("fruitninja").onclick = function() {
 // document.getElementById("vp").setAttribute("max-width: ");
 
 const player = document.getElementById("vp");
+const login = document.getElementById("login-btn");
+let username;
+let password;
+
+login.onclick = function() {
+    username = document.getElementById("usr");
+    if(username === "") {
+        window.alert("Username field cannot be empty!");
+    }
+    password = document.getElementById("pwd");
+    if(password === "") {
+        window.alert("Password field cannot be empty!");
+    }
+}
