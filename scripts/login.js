3c6e1d75c7a84a943a2549893a5ec1f3456a077d
SmoCloud <psn.power@yahoo.com>
2024-10-10 10:27:09 -0400

    minor change to if logic in login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,7 @@
 let username;   // stores the entered username
 let password;   // stores the entered password
 
+
 // get element for login button
 const loginBtn = document.getElementById("login-btn");
 
@@ -8,22 +9,21 @@ const loginBtn = document.getElementById("login-btn");
 loginBtn.onclick = function() {
     username = document.getElementById("usr").value;    // get entered username
     console.log(username);  // log username to console for testing
+    password = document.getElementById("pwd").value;    // get entered password
+    console.log(password);  // log password to console for testing
 
     // while username field is empty
     if(username === ""|| username === null) {
         window.alert("Username field cannot be empty!");    // alert that username cannot be empty
     }
 
-    password = document.getElementById("pwd").value;    // get entered password
-    console.log(password);  // log password to console for testing
-
     // while password field is empty
-    if(password === "" || password === null) {
+    else if(password === "" || password === null) {
         window.alert("Password field cannot be empty!");    // alert that password cannot be empty
     }
 
     // if username and password both not empty
-    if (username && password) {
+    else {
         window.alert("Username and password fields were filled.");  // alert for testing purposes
         window.location.href = "index.html";    // change window location to home page
     }
